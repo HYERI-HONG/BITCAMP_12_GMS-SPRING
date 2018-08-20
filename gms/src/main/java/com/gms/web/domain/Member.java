a8a7286a091d5eb0294df3139d57e9caf942df99
HYERI-HONG <rusa12615059@gmail.com>
2018-08-20 18:32:43 +0900

    el setting

---
+++ gms/src/main/java/com/gms/web/domain/Member.java
@@ -0,0 +1,10 @@
+package com.gms.web.domain;
+
+import org.springframework.stereotype.Component;
+import lombok.Data;
+
+@Component
+@Data
+public class Member {
+	private String userId,ssn,name,roll,teamId,password,age,subject,gender;
+}
